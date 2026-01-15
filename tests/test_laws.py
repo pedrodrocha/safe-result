bd37125047ea8c600cde7622ddd5f11dec180f33
Pedro Diniz Rocha <pedro.diniz.rocha@gmail.com>
2026-01-15 11:23:51 -0300

    functor laws test

--- tests/test_laws.py
+++ tests/test_laws.py
@@ -82,3 +82,68 @@ class TestMonadLaws:
 
             if left.is_err() and right.is_err():
                 assert left.unwrap_err() == right.unwrap_err() == "f error"
+
+class TestFunctorLaws:
+    """
+    For a proper implementation of a Functor, we need to ensure that it satisfies the following laws:
+        1. Identity: fmap id  ≡  id
+        2. Composition: fmap (f . g)  ≡  fmap f . fmap g
+
+    In Result terms:
+        - fmap = map
+        - id = lambda x: x
+        - (f . g)(x) = f(g(x))
+    
+    """
+
+    class TestIdentity:
+        # m.map(x => x) ≡ m
+        def test_it_holds_for_ok(self):
+            m = Result.ok(42)
+            result = m.map(lambda x: x)
+
+            assert result.unwrap() == m.unwrap()
+
+        def test_it_holds_for_err(self):
+            m = Result.err("error")
+            result = m.map(lambda x: x)
+
+            assert result.is_err() is True
+
+            if result.is_err():
+                assert result.unwrap_err() == "error"
+
+    class TestComposition:
+        # m.map(f . g) ≡ m.map(g).map(f)
+        def test_it_holds_for_ok(self):
+            def f(x: int) -> int:
+                return x * 2
+
+            def g(x: int) -> int:
+                return x + 10
+
+            m = Result.ok(5)
+
+            left = m.map(lambda x: f(g(x)))
+            right = m.map(g).map(f)
+
+            assert left.unwrap() == right.unwrap()
+            assert left.unwrap() == 30  # (5 + 10) * 2 = 30
+
+        def test_it_holds_for_err(self):
+            def f(x: int) -> int:
+                return x * 2
+
+            def g(x: int) -> int:
+                return x + 10
+
+            m = Result.err("error")
+
+            left = m.map(lambda x: f(g(x)))
+            right = m.map(g).map(f)
+
+            assert left.is_err() is True
+            assert right.is_err() is True
+
+            if left.is_err() and right.is_err():
+                assert left.unwrap_err() == right.unwrap_err() == "error"
